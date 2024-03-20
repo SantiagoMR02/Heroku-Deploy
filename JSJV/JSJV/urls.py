0bbdd76ef71c98122d94b875537c0a4be7f39082
Julian Santiago Millan Rodriguez <juliansanti1202@gmail.com>
2024-03-20 18:39:50 -0500

    Update urls.py

--- JSJV/JSJV/urls.py
+++ JSJV/JSJV/urls.py
@@ -33,6 +33,7 @@ urlpatterns = [
     path('PDFot/', PDFot.as_view(), name='PDFot'),
 ] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 
-#urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
 
 
